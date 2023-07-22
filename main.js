0bf94523749432101cd5156ad82b2067b27d6eee
Beksulton Po'latov <beksulton753@gmail.com>
2023-07-22 18:46:44 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+var num1 = 10 
+var num2 = 5 
+
+
+var result = num1 + num2;
+
+console.log(result);
+
+
+
+var a = 25
+var b = 15
+
+var result = a - b 
+
+console.log(result)
+
+
+var a = 10
+var b = 5
+var d = 7
+
+var result = a - b
+var result = result * 7
+
+console.log(result)
+
+
+var a = 10
+var b = 3 
+var result = 10 % 3
+
+console.log(result)
+
+
+
+var a = 10
+var b = 2
+
+var result = 2 ** 10 
+console.log(result)
+
+
